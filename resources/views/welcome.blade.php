b2d2b02de798ead65df1366003d78c1bfd7f14ce
Falua Temitope Oyewole <emmat0616@gmail.com>
2018-05-23 18:10:00 +0100

    working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
     <section class = "landing">
+        <div class = "overlay"></div>
         <div class="inner-landing">
             <div class="col-3">
                 <div class="content">
-                    <span></span>
+                    <span>Lorem </span>
+                    Something is here
+                    Loading
                 </div>
             </div>
             <div class="col-5">
@@ -15,7 +18,7 @@
     </section>
 @endsection
 
-@section('content')
+@section('feature')
     <section class = "feature">
         <div>
             Inner feature
